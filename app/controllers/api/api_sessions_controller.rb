d65f40902e91fa6334bb7b32dc5cc270e52e9382
JD Romero <ncsuguy04@gmail.com>
2019-05-28 17:06:11 -0400

    Second First Commit

---
+++ app/controllers/api/api_sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+        if @user
+            login!(@user)
+            render :show
+        else
+            render json: ['Invalid Username/Password'], status: 422
+        end
+        
+    end
+
+    def destroy
+        if current_user
+            logout!
+            render json: {}
+        else
+            render json: ['Login to logout'], status: 404
+        end
+    end
+
+end
