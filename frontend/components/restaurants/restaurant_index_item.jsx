ca487ae5166bdfcdd855d184f700487751c6f7a1
JD Romero <ncsuguy04@gmail.com>
2019-06-03 18:05:28 -0400

    Add Edits

--- frontend/components/restaurants/restaurant_index_item.jsx
+++ frontend/components/restaurants/restaurant_index_item.jsx
@@ -5,26 +5,13 @@ const RestaurantIndexItem = ({restaurant}) => {
     // debugger
     let rand = Math.floor(Math.random() * 1000)
     return (
-<<<<<<< HEAD
-        <ul>
-=======
        
         <ul className='rest-index-items'><Link to={`/restaurants/${restaurant.id}`} className='rest-index-names'>{restaurant.name}</Link>
->>>>>>> restaurantBranch
             <li>
                 <i class="fa fa-star" aria-hidden="true"></i> 
                 <i class="fa fa-star" aria-hidden="true"></i>
                 <i class="fa fa-star" aria-hidden="true"></i>
                 <i class="fa fa-star" aria-hidden="true"></i>
-<<<<<<< HEAD
-                (202)
-            </li>
-           <li>6:30 PM</li>
-           <li>6:45 PM</li>
-           <li>7:00 PM</li>
-           <li>7:15 PM</li>
-           <li></li>
-=======
                 ({rand})
             </li>
             <li className="tag" >{restaurant.tag}</li>
@@ -35,7 +22,6 @@ const RestaurantIndexItem = ({restaurant}) => {
                 <li>6:45 PM</li>
                 <li>7:00 PM</li>
            </ul>
->>>>>>> restaurantBranch
         </ul>
     )
 }
