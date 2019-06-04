f9f96f0041aa4f5cc6a720c99099a81eb1415ccf
JD Romero <ncsuguy04@gmail.com>
2019-06-04 09:35:22 -0400

    Style Restaurant Show Page

--- frontend/components/restaurants/restaurant.jsx
+++ frontend/components/restaurants/restaurant.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import RestaurantIndexItem from './restaurant_index_item';
-
+import NavbarContainer from '../navbar/nav_bar_container';
 
 class Restaurant extends React.Component {
     constructor(props) {
@@ -26,6 +26,7 @@ class Restaurant extends React.Component {
 
         return (
             <div className="rest-container">
+                <NavbarContainer />
                 <div className="rest-list">
                     <ul className='rest-ul'>
                         {listRest}
--- frontend/components/restaurants/restaurant_show.jsx
+++ frontend/components/restaurants/restaurant_show.jsx
@@ -16,62 +16,73 @@ class RestaurantShow extends React.Component {
         return (
            
                 
-        
-            <div className='rest-show-div'>
-                <NavbarContainer />
-                <div className="rest-show-nav-container">
-                    <nav className='rest-show-nav'>
-                        <ul className='rest-show-ul-nav'>
-                            <li>
-                                Overview
-                            </li>
-                            <li>
-                                Photos
-                            </li>
-                            <li>
-                                Menu
-                            </li>
-                            <li>
-                                Reviews
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className="restaurant-info">
-                    <h1>{this.props.restaurant.name}</h1>
-                    <ul>
-                        <li>{this.props.restaurant.tag}</li>
-                    </ul>
-                    <div className="rest-aboutme">
-                        <p className='p-aboutme'>
-                            {this.props.restaurant.aboutme}
-                        </p> 
+            <div className='rest-show-parent'>
+                <div className='rest-show-div'>
+                    <NavbarContainer />
+                    <div className="rest-show-nav-container">
+                        <nav className='rest-show-nav'>
+                            <ul className='rest-show-ul-nav'>
+                                <li>
+                                    Overview
+                                </li>
+                                <li>
+                                    Photos
+                                </li>
+                                <li>
+                                    Menu
+                                </li>
+                                <li>
+                                    Reviews
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
-                        <div className="rest-show-photos">
-                            <h2>Photos</h2>
+                    <div className="restaurant-info">
+                        <h1>{this.props.restaurant.name}</h1>
+                        <ul>
+                            <li>{this.props.restaurant.tag}</li>
+                        </ul>
+                        <div className="rest-aboutme">
+                            <p className='p-aboutme'>
+                                {this.props.restaurant.aboutme}
+                            </p> 
                         </div>
-                        <div className="rest-show-menu">
-                            <h2>Menu</h2>
-                        </div>              
+                            <div className="rest-show-photos">
+                                <h2>Photos</h2>
+                            </div>
+                            <div className="rest-show-menu">
+                                <h2>Menu</h2>
+                            </div>              
+                    </div>
+                    
+                    <div className="rest-show-review">
+                        <h2>What People Are Saying</h2>
+                    </div>
+
                 </div>
                 <div className='rest-reservation-container'>
-                    <h2>Make a Reservation</h2>
+                        <div className='rest-show-h2'><h2>Make a reservation</h2></div> 
                     <div className="rest-reserve-options">
-                        <label for="rest-reserve-party"></label>
-                        <select name="rest-reserve-party" id="rest-reserve-party" className="rest-reserve-party">
-                            <option value="2 people">For 2 people</option>
-                        </select>
-                        <label for="rest-reserve-date">Date</label>
-                        <input className='rest-reserve-date' type="date" />
-                        <label for="rest-reserve-time">Time</label>
-                        <input className="rest-reserve-time" type="time" />
-
+                        <div className='rest-reserve-party-container'>
+                            <label for="rest-reserve-party-select">Party Size</label>
+                            <select name="rest-reserve-party-select" id="rest-reserve-party-select" className="rest-reserve-party-select">
+                                <option value="2 people">For 2</option>
+                            </select>
+                        </div>
+                        <dir className="rest-reserve-datetime-container">
+                            <div className="rest-reserve-date-container">  
+                                <label for="rest-reserve-date">Date</label>
+                                <input className='rest-reserve-date' type="date" />
+                            </div> 
+                            <div className="rest-reserve-time-container">
+                                <label for="rest-reserve-time">Time</label>
+                                <select className="rest-reserve-time" > 
+                                    <option value="7pM">7:00 PM</option>
+                                </select>
+                             </div>
+                        </dir>
                     </div>
                 </div>
-                <div className="rest-show-review">
-                    <h2>What People Are Saying</h2>
-                </div>
-
             </div>
         )
     }
