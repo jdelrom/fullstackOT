05eb7607cbf5add778173a1495cdb1e1a3babf19
JD Romero <ncsuguy04@gmail.com>
2019-06-06 06:30:00 -0400

    Add Reservation Action to User Reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_RESERVATION } from '../actions/reservation_actions';
 import { merge } from 'lodash';
 
 const usersReducer = (state = {}, action) => {
@@ -9,6 +10,13 @@ const usersReducer = (state = {}, action) => {
                 [action.currentUser.id]: action.currentUser
             }
             return merge({}, state, newState)
+        case RECEIVE_RESERVATION:
+            // debugger
+            const nwState = merge({}, state);
+            // debugger
+            nwState[action.reservation.user_id].reservations = [action.reservation.user_id]
+            // debugger
+            return merge({}, state, nwState)
         default:
             return state 
     }
