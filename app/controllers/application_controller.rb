e112182f22129cb7ebac9c88ec3b67b112775f87
JD Romero <ncsuguy04@gmail.com>
2019-09-09 11:29:08 -0400

    Add Protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
+    protect_from_forgery
 
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
